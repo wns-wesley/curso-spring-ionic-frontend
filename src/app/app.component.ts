9c855d225cedf87c642b13ff768dd032be9af0d2
Wesley <wns.wesley@gmail.com>
2018-06-19 18:58:05 -0300

    Lazy Loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,16 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: string = 'HomePage';
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{title: string, component: string}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage }
+      { title: 'Home', component: 'HomePage' }
     ];
 
   }
