ff7a35a0b929203c186824f9ad87c90d178cc314
Wesley <wns.wesley@gmail.com>
2018-06-23 20:54:21 -0300

    Salvando os dados do usuário logado no localStorage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -33,7 +33,8 @@ export class HomePage {
   login() {
     this.auth.authenticate(this.creds)
       .subscribe(response => {
-        console.log(response.headers.get('Authorization'));
+        //console.log(response.headers.get('Authorization'));
+        this.auth.successfulLogin(response.headers.get('Authorization'));
         this.navCtrl.setRoot('CategoriasPage');
       },
       error => {});
